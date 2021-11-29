5c5112b00d19624c45f60a716e4f177cbdb067b2
Davidson Bpe <davidsomixer@gmail.com>
2021-11-29 12:07:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const contents = fs.readFileSync(__dirname + '/index.html');
 http.createServer((req, res) => {
     res.end(contents);
 }).listen(3100);
+console.log('listening on http://localhost:3100/');
